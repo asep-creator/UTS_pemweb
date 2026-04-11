389792d7d9403115bcbadd335a33b4003e126f6d
Adina <dina40612@gmail.com>
2026-04-11 15:22:45 +0700

    update tahun

--- galeri.php
+++ galeri.php
@@ -56,9 +56,9 @@ class="p-2 border rounded w-1/3">
 <option value="2021">2021</option>
 <option value="2020">2020</option>
 <option value="2019">2019</option>
-<option value="2021">2018</option>
-<option value="2021">2017</option>
-<option value="2021">2016</option>
+<option value="2018">2018</option>
+<option value="2017">2017</option>
+<option value="2016">2016</option>
 </select>
 
 <select id="harga" onchange="loadData()" class="p-2 border rounded">
